1aed713f1459c60811b8107d77e3c12acf15d3f6
Samonto Karmaker <samonto0726@gmail.com>
2023-05-21 21:38:50 +0600

    Actors model setup

---
+++ models/actors.js
@@ -0,0 +1,44 @@
+//External imports
+const mongoose = require("mongoose")
+
+//Schema Design
+const actorSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            require: true,
+            lowercase: true,
+            trim: true
+        },
+        moblie: {
+            type: String,
+            require: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            require: true
+        },
+        avatar: {
+            type: String
+        },
+        role: {
+            type: String,
+            enum: ["admin", "user"],
+            default: "user"
+        },
+        record: {
+            timestamps: true
+        }
+    }
+)
+
+//Model Creation
+const actors = mongoose.model("actors", actorSchema)
+
+module.exports = actors
