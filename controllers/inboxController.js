2c2b0171ab37c3f7d64a69fa32187731831a04a9
Samonto Karmaker <samonto0726@gmail.com>
2023-06-22 23:33:21 +0600

    get inbox info implemented to load conversations

--- controllers/inboxController.js
+++ controllers/inboxController.js
@@ -1,5 +1,22 @@
-const getInboxInfo = (req, res, next) => {
-    res.render("inbox")
+//External imports
+
+//Internal imports
+const conversations = require("../models/conversations")
+
+const getInboxInfo = async (req, res, next) => {
+    try{
+        const conversation = await conversations.find({
+            $or: [
+                {"creator.id": req.user.userid},
+                {"participant.id": req.user.userid}
+            ]
+        })
+        res.locals.data = conversation
+        res.render("inbox")
+    }
+    catch(err){
+        next(err)
+    }
 }
 
 module.exports = {
