ac5f9330f06bc31e299183c39890ab613c647ae8
Samonto Karmaker <samonto0726@gmail.com>
2023-07-02 22:21:47 +0600

    Adder role based authorization

--- middlewares/common/protectPages.js
+++ middlewares/common/protectPages.js
@@ -1,9 +1,10 @@
 //External Import
 const jwt = require("jsonwebtoken")
+const createError = require("http-errors")
 
-//If user trys to go "/" after login, redirect him to inbox
+//If user tries to go "/" after login, redirect him to inbox
 const redirectLoggedIn = (req, res, next) => {
-    //If user is no loggedin, cookies = null
+    //If user is no logged in, cookies = null
     let cookies = Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null
     if(cookies){
         res.redirect("/inbox")
@@ -13,7 +14,7 @@ const redirectLoggedIn = (req, res, next) => {
     }
 }
 
-//If user trys to go the "/user" or "/inbox", we need to check if he is logged in
+//If user tries to go the "/user" or "/inbox", we need to check if he is logged in
 const checkLogin = (req, res, next) => {
     let cookies = Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null
     if(cookies){
@@ -61,7 +62,33 @@ const checkLogin = (req, res, next) => {
     }
 }
 
+//Check if user is authorized to access the route
+const roleAuth = roles => {
+    return (req, res, next) => {
+        if(roles.includes(req.user.role)){
+            next()
+        }
+        else{
+            if(res.locals.html){
+                //For HTML response
+                next(createError(401, "Not Authorized!"))
+            }
+            else{
+                //For API response
+                res.status(401).json({
+                    errors: {
+                        common: {
+                            msg: "Not Authorized!"
+                        }
+                    }
+                })
+            }
+        }
+    }
+}
+
 module.exports = {
     redirectLoggedIn,
-    checkLogin
+    checkLogin,
+    roleAuth
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,16 +6,16 @@ const {getUsers, addUser, removeUser} = require("../controllers/userController")
 const addTitle2HTMLResponse = require("../middlewares/common/decorate_html_reponse")
 const avatarUploader = require("../middlewares/users/avatar_uploader")
 const { addUserValidators, validationResultHandler } = require("../middlewares/users/user_validator")
-const { checkLogin } = require("../middlewares/common/protectPages")
+const { checkLogin, roleAuth } = require("../middlewares/common/protectPages")
 
 //Initializing Router
 const router = express.Router()
 
 //Get Controller
-router.get("/", addTitle2HTMLResponse("Users"), checkLogin, getUsers)
+router.get("/", addTitle2HTMLResponse("Users"), checkLogin, roleAuth(["admin"]), getUsers)
 
 //Add users
-router.post("/", checkLogin, avatarUploader, addUserValidators, validationResultHandler, addUser)
+router.post("/", checkLogin, roleAuth(["admin"]), avatarUploader, addUserValidators, validationResultHandler, addUser)
 
 //Remove users
 router.delete("/:id", removeUser)
